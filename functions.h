b250469d65ec21ebbc69ff8d8f3b2129eb1d9db8
MarceronT <58844411+MarceronT@users.noreply.github.com>
2020-01-27 10:04:58 +0100

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+#include <stdint.h>
+
+void fenetre(uint8_t character_h,uint8_t character_v);
+void menu();
+void monstres();
+
+#endif /* FUNCTIONS_H_ */
