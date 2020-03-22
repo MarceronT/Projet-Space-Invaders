c147293d509f3da8a03613c4220015fba66a2762
MarceronT <58844411+MarceronT@users.noreply.github.com>
2020-03-22 15:30:59 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -2,13 +2,24 @@
 #define FUNCTIONS_H_
 #include <stdint.h>
 
-void fenetre(uint8_t character_h,uint8_t character_v);
-void menu();
-void barre();
-void tir();
-void sleep(uint32_t n);
-void ini_monstres();
-void monstres();
-void hit_box();
+void window(uint8_t character_h,uint8_t character_v);
+void menu(void);
+void ini_life(void);
+void ini_ship(void);
+void ini_shot(void);
+void ini_shot_monster(void);
+void ini_monster(void);
+void ini_blocs(void);
+uint8_t life(void);
+void sleep(unsigned long n);
+void ship(void);
+void shot(void);
+void hit_box(void);
+void all_monster(void);
+void shot_one_monster(uint8_t num_monstre,uint8_t num_lignes);
+void shot_all_monster(uint8_t num_monstre);
+void hit_box_monster(void);
+uint8_t end (void);
 
 #endif /* FUNCTIONS_H_ */
+
